1711ae9a76112e654a27d181274798d23012fb39
699nb <bayanurbaev@mail.ru>
2023-02-17 19:16:30 +0600

    add the sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Container from "./container/Container";
 import { Forms } from "./forms";
 import Image from "./image/Image";
 import Loader from "./loader/Loader";
+import Sidebar from "./sidebar/Sidebar";
 
 
 
@@ -9,5 +10,6 @@ export const Components = {
     Container,
     Image,
     Forms,
-    Loader
+    Loader,
+    Sidebar
 };
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import cls from "./Sidebar.module.scss"
+import { Components } from '..';
+import Logo from "../../assets/images/logo/InstaLogo.png"
+import cls from "../../assets/styles/sidebar/Sidebar.module.scss"
 
 export default function Sidebar() {
     return (
-        <section className={cls}>
-            
+        <section className={cls.sidebar_wrapper}>
+            <div className={cls.sidebar_wrapper_content}>
+                <Components.Image src={Logo}/>
+                
+            </div>
         </section>
     )
 };
